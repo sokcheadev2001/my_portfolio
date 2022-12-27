5eba3b10c68537d8cb87c7fc07a304d8b299d76a
sokcheadev2001 <sokchealeang.dev@gmail.com>
2022-12-27 13:40:06 +0700

    finished ci/cd

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function HomePage() {
   return (
     <div>
-      <h2 className='text-center text-2xl text-teal-600'>
-        Back to Digital Ocean
-      </h2>
+      <h2 className='text-center text-2xl text-teal-600'>Nice</h2>
     </div>
   );
 }
