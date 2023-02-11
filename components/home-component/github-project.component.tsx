cf10db77df53db678c053d709d073969bbd18efb
sokcheadev2001 <sokchealeang.dev@gmail.com>
2023-02-11 23:12:07 +0700

    add project box component & fix color on bright mode

--- components/home-component/github-project.component.tsx
+++ components/home-component/github-project.component.tsx
@@ -1,11 +1,16 @@
 import { FC } from "react";
 import { motion } from "framer-motion";
 import { Section } from "../framer-motion/motion.component";
-import { MdFolderOpen } from "react-icons/md";
-import { SiGithub } from "react-icons/si";
-import { FaExternalLinkAlt } from "react-icons/fa";
-import { RiReactjsFill } from "react-icons/ri";
+import ProjectBox from "../projectbox/project-box.component";
+import "../../project.json";
 const Project: FC = () => {
+  const Project = {
+    projectUrl: "https://github.com/sokcheadev2001/React-Store.git",
+    projectDemo: "",
+    projectTitle: "React Store",
+    projectDescription: "Build store front using react js and firebase",
+    projectLanguages: "React Js, Firebase",
+  };
   return (
     <Section>
       <div className='md:mt-24 mt-16'>
@@ -20,92 +25,7 @@ const Project: FC = () => {
           </h2>
         </motion.div>
         <ul className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 mt-10'>
-          <motion.li
-            className='dark:bg-lightPrimary bg-darkGray py-8 px-7 rounded-xl'
-            whileHover={{
-              scale: 1.05,
-              transition: { type: "spring", stiffness: 400, damping: 10 },
-            }}
-          >
-            <div className='dark:bg-primary/20 bg-secondary py-5 flex justify-between'>
-              <div>
-                <MdFolderOpen className='text-3xl dark:text-teal-400 text-gray-800 dark:hover:text-white' />
-              </div>
-              <span className='flex gap-4 items-center'>
-                <SiGithub className='text-xl dark:text-brightGray text-gray-800 dark:hover:text-white' />
-                <FaExternalLinkAlt className='text-xl dark:text-brightGray text-gray-800 dark:hover:text-white' />
-              </span>
-            </div>
-            <div className='md:text-xl dark:text-brightGray text-lightGray mb-3'>
-              React Monster
-            </div>
-            <div className='text-sm dark:text-darkGray text-lightGray pb-10'>
-              A simple react app that fetching api and render with search method
-              to find specific monster
-            </div>
-            <div className='flex justify-between'>
-              <RiReactjsFill className='text-2xl text-cyan-500' />
-            </div>
-          </motion.li>
-          <motion.li
-            className='dark:bg-lightPrimary bg-darkGray py-8 px-7 rounded-xl'
-            whileHover={{
-              scale: 1.05,
-              transition: { type: "spring", stiffness: 400, damping: 10 },
-            }}
-          >
-            <div className='dark:bg-primary/20 bg-secondary py-5 flex justify-between'>
-              <div>
-                <MdFolderOpen className='text-3xl dark:text-teal-400 text-gray-800 dark:hover:text-white' />
-              </div>
-              <span className='flex gap-4 items-center'>
-                <SiGithub className='text-xl dark:text-brightGray text-gray-800 dark:hover:text-white' />
-                <FaExternalLinkAlt className='text-xl dark:text-brightGray text-gray-800 dark:hover:text-white' />
-              </span>
-            </div>
-            <div className='md:text-xl dark:text-brightGray text-lightGray mb-3'>
-              React Monster
-            </div>
-            <div className='text-sm dark:text-darkGray text-lightGray pb-10'>
-              A simple react app that fetching api and render with search method
-              to find specific monster
-            </div>
-            <div className='flex justify-between'>
-              <RiReactjsFill className='text-2xl text-cyan-500' />
-            </div>
-          </motion.li>
-          <motion.li
-            className='dark:bg-lightPrimary bg-darkGray py-8 px-7 rounded-xl'
-            whileHover={{
-              scale: 1.05,
-              transition: { type: "spring", stiffness: 400, damping: 10 },
-            }}
-          >
-            <div className='dark:bg-primary/20 bg-secondary py-5 flex justify-between'>
-              <div>
-                <MdFolderOpen className='text-3xl dark:text-teal-400 text-gray-800 dark:hover:text-white' />
-              </div>
-              <span className='flex gap-4 items-center'>
-                <SiGithub className='text-xl dark:text-brightGray text-gray-800 dark:hover:text-white' />
-                <FaExternalLinkAlt className='text-xl dark:text-brightGray text-gray-800 dark:hover:text-white' />
-              </span>
-            </div>
-            <div className='md:text-xl dark:text-brightGray text-lightGray mb-3'>
-              React Monster
-            </div>
-            <div className='text-sm dark:text-darkGray text-lightGray pb-10'>
-              A simple react app that fetching api and render with search method
-              to find specific monster
-            </div>
-            <div className='flex justify-between'>
-              <RiReactjsFill className='text-2xl text-cyan-500' />
-            </div>
-          </motion.li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
-          <li>6</li>
-          <li>7</li>
+          <ProjectBox data={Project} />
         </ul>
       </div>
     </Section>
--- components/projectbox/project-box.component.tsx
+++ components/projectbox/project-box.component.tsx
@@ -1,43 +1,62 @@
 import { motion } from "framer-motion";
-import React from "react";
+import Link from "next/link";
+import React, { FC } from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { MdFolderOpen } from "react-icons/md";
-import { RiReactjsFill } from "react-icons/ri";
 import { SiGithub } from "react-icons/si";
 
-const ProjectBox = () => {
-  const arrs = ["RiReactjsFill"];
-  const loop = () => {
-    const listItems = arrs.map((number) => <li key={number}>{number}</li>);
-    return listItems;
+interface Project {
+  data: {
+    projectUrl: string;
+    projectDemo?: string;
+    projectTitle: string;
+    projectDescription: string;
+    projectLanguages: string;
   };
+}
+const ProjectBox = ({
+  data: {
+    projectUrl,
+    projectDemo,
+    projectTitle,
+    projectDescription,
+    projectLanguages,
+  },
+}: Project) => {
   return (
     <motion.li
-      className='dark:bg-lightPrimary bg-darkGray py-8 px-7 rounded-xl'
+      className='dark:bg-lightPrimary bg-gray-100/80 py-8 px-7 rounded-xl'
       whileHover={{
-        scale: 1.1,
+        scale: 1.05,
         transition: { type: "spring", stiffness: 400, damping: 10 },
       }}
     >
-      <div className='dark:bg-primary/20 bg-secondary py-5 flex justify-between'>
+      <div className='dark:bg-primary/20 bg-gray-200/30 py-5 flex justify-between'>
         <div>
           <MdFolderOpen className='text-3xl dark:text-teal-400 text-gray-800 dark:hover:text-white' />
         </div>
         <span className='flex gap-4 items-center'>
-          <SiGithub className='text-xl dark:text-brightGray text-gray-800 dark:hover:text-white' />
-          <FaExternalLinkAlt className='text-xl dark:text-brightGray text-gray-800 dark:hover:text-white' />
+          <Link href={projectUrl} aria-label='github url'>
+            <SiGithub className='text-xl dark:text-brightGray text-gray-800 dark:hover:text-white' />
+          </Link>
+          {projectDemo !== undefined && projectDemo !== "" ? (
+            <Link href={projectDemo} aria-label='project demo url'>
+              <FaExternalLinkAlt className='text-xl dark:text-brightGray text-gray-800 dark:hover:text-white' />
+            </Link>
+          ) : (
+            ""
+          )}
         </span>
       </div>
       <div className='md:text-xl dark:text-brightGray text-lightGray mb-3'>
-        React Monster
+        {projectTitle}
       </div>
       <div className='text-sm dark:text-darkGray text-lightGray pb-10'>
-        A simple react app that fetching api and render with search method to
-        find specific monster
+        {projectDescription}
       </div>
-      <div className='flex justify-between'>
-        <RiReactjsFill className='text-2xl text-cyan-500' />
-      </div>
+      <p className='text-xs dark:text-darkGray text-lightGray'>
+        {projectLanguages}
+      </p>
     </motion.li>
   );
 };
