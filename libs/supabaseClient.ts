a71075bfc38098555bff1c8b9df9b5e2ef48a75d
sokcheadev2001 <sokchealeang.dev@gmail.com>
2023-02-15 21:30:54 +0700

    fix supabse env name

--- libs/supabaseClient.ts
+++ libs/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.SUPERBASE_URL as string,
+  process.env.SUPABASE_URL as string,
   process.env.SUPABASE_ANON_KEY as string
 );
